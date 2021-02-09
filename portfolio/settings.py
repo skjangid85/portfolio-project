bc739d960109816c01d9d8ebe6ab1a7d9f17cae0
Sohan <skjangid85@gmail.com>
2021-02-09 12:36:35 +0530

    Blog added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
+    'blogs.apps.BlogsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
